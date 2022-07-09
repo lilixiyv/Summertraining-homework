329103e9c3e671453a5630a81fe9ee6ac86abb93
Ther <1329438302@qq.com>
2022-07-09 12:35:24 +0800

    chore: add hw

--- homework/src/main.cpp
+++ homework/src/main.cpp
@@ -10,18 +10,6 @@ int main()
     // TODO 2: initialize the object of student
     player::Player player1;
 
-    player1.set_life(10000);
-    player1.set_nickname("Alice");
-    player1.set_job(player::Job::SHOOTER);
-
-    player::Prop* prop1 = player1.add_props();
-    prop1 ->set_cd(1000);
-    prop1 ->set_name("SuperFast");
-
-    player::Prop* prop2 = player1.add_props();
-    prop2 ->set_cd(10);
-    prop2 ->set_name("NuclearWeapon");
-
     // see the debug string
     std::cout << player1.DebugString() << std::endl;
 
